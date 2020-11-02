3d1d7cb50ab3d52ccf35c065ebe9660faca83903
AMR <73802610+amr-ff-dev@users.noreply.github.com>
2020-11-02 08:34:46 -0700

    #2 added initial tilemap

---
+++ moonshot-titan/levels/player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	var movement_vector = input_vector.normalized()
+	move_and_collide(movement_vector * delta * 100)
