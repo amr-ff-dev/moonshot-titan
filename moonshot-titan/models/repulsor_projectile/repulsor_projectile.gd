877c37db8d1a57a24004829a41559f7acc9496a9
AMR <73802610+amr-ff-dev@users.noreply.github.com>
2020-11-04 13:28:37 -0700

    #10 destroy enemy when shot

--- moonshot-titan/models/repulsor_projectile/repulsor_projectile.gd
+++ moonshot-titan/models/repulsor_projectile/repulsor_projectile.gd
@@ -7,6 +7,8 @@ var normalized_direction = Vector2.ZERO
 func _physics_process(delta):
 	var collision_info = move_and_collide(normalized_direction * delta * speed)
 	if collision_info:
+		if "Spider" == collision_info.collider.name:
+			collision_info.collider.free()
 		queue_free()
 
 func launch(origin, target):
