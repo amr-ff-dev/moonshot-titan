37ec9adf770a3c1f2af0246736ed4347a7490ab0
AMR <73802610+amr-ff-dev@users.noreply.github.com>
2020-11-05 20:19:28 -0700

    #17 apply linear interpolation to player starting/stopping movement

--- moonshot-titan/characters/player/player.gd
+++ moonshot-titan/characters/player/player.gd
@@ -2,27 +2,33 @@ extends KinematicBody2D
 
 signal shoot(Projectile, origin, direction)
 
-export (int) var movement_speed = 200
+export(int) var speed = 200
+export(float, 0, 1, 0.1) var accelerate = 0.2
 
 var RepulsorProjectile = preload("res://models/repulsor_projectile/repulsor_projectile.tscn")
 
-var direction = Vector2.ZERO
-var speed = 0
+var velocity = Vector2.ZERO
+var desired_velocity = Vector2.ZERO
 var recoiling = false
 
 func _physics_process(delta):
-	var collision = move_and_collide(direction * speed * delta)
+	velocity = velocity.linear_interpolate(desired_velocity, accelerate)
+	var collision = move_and_collide(velocity * delta)
 	if collision and collision.collider.get_collision_layer() == Titan.CollisionLayers.WALL:
-		direction = Vector2.ZERO
-		speed = 0
+		velocity = Vector2.ZERO
+		desired_velocity = Vector2.ZERO
 		recoiling = false
 
 func _input(event):
-	if !recoiling:
-		direction = get_normalized_input_vector()
-		speed = movement_speed
 	if event.is_action_pressed("shoot"):
 		shoot()
+	else:
+		update_desired_velocity()
+
+func update_desired_velocity():
+	if recoiling:
+		return
+	desired_velocity = get_normalized_input_vector() * speed
 
 func get_normalized_input_vector():
 	var input_vector = Vector2.ZERO
@@ -32,11 +38,10 @@ func get_normalized_input_vector():
 
 func shoot():
 	var target = get_global_mouse_position()
-	var direction = position.direction_to(target).normalized()
+	var direction_to_target = position.direction_to(target).normalized()
 	var projectile = RepulsorProjectile.instance()
 	
-	emit_signal("shoot", projectile, position, direction)
+	emit_signal("shoot", projectile, position, direction_to_target)
 	
-	self.direction = -direction
-	self.speed = projectile.speed
-	self.recoiling = true
+	desired_velocity = -direction_to_target * projectile.speed
+	recoiling = true
