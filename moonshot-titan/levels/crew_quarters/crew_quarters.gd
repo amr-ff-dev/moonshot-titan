237c72e782923e2de73fd94c20dcf314a201e6f6
AMR <73802610+amr-ff-dev@users.noreply.github.com>
2020-11-08 18:03:18 -0700

    #33, #34 reorganized to have main titan scene and separate scenes per room

--- moonshot-titan/levels/crew_quarters/crew_quarters.gd
+++ moonshot-titan/levels/crew_quarters/crew_quarters.gd
@@ -1,15 +1,6 @@
 extends Node2D
 
-class_name Titan
-
-enum CollisionLayers {
-	WALL = 1,
-	PLAYER = 2,
-	ENEMY = 4,
-	PROJECTILE = 8,
-	HAZARD = 16,
-	OBSTACLE = 32,
-}
+class_name CrewQuarters
 
 onready var player = $Player
 onready var restart_button = $HUD/Restart
---
+++ moonshot-titan/levels/titan/titan.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+class_name Titan
+
+enum CollisionLayers {
+	WALL = 1,
+	PLAYER = 2,
+	ENEMY = 4,
+	PROJECTILE = 8,
+	HAZARD = 16,
+	OBSTACLE = 32,
+}
