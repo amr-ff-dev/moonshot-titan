77d8817dc256ecfa81f9a110c116b7c733be781b
AMR <73802610+amr-ff-dev@users.noreply.github.com>
2020-11-11 17:46:53 -0700

    #45 move player into main Titan scene

--- moonshot-titan/levels/crew_quarters/crew_quarters.gd
+++ moonshot-titan/levels/crew_quarters/crew_quarters.gd
@@ -1,18 +1,3 @@
 extends Node2D
 
 class_name CrewQuarters
-
-onready var player = $Player
-onready var restart_button = $HUD/Restart
-onready var player_respawn = $PlayerRespawn
-
-func _on_Player_shoot(projectile, origin, direction):
-	add_child(projectile)
-	projectile.launch(origin, direction)
-
-func _on_Player_game_over():
-	restart_button.visible = true
-
-func _on_Restart_pressed():
-	player.respawn(player_respawn)
-	restart_button.visible = false
--- moonshot-titan/levels/titan/titan.gd
+++ moonshot-titan/levels/titan/titan.gd
@@ -11,3 +11,18 @@ enum CollisionLayers {
 	PLAYER_HAZARD = 32,
 	OBSTACLE = 64,
 }
+
+onready var player = $Player
+onready var restart_button = $HUD/Restart
+onready var player_respawn = $CrewQuarters/PlayerRespawn
+
+func _on_Player_shoot(projectile, origin, direction):
+	add_child(projectile)
+	projectile.launch(origin, direction)
+
+func _on_Player_game_over():
+	restart_button.visible = true
+
+func _on_Restart_pressed():
+	player.respawn(player_respawn)
+	restart_button.visible = false
