3ec8c8c80117cd68cec04cbb111d4566086710a6
mng <73802596+mng-ff-dev@users.noreply.github.com>
2020-11-14 13:14:04 -0700

    #53 spiders in oxygenator were not set to collide with obstacles.
    Converted computer core to quest room
    Updated quest text for storage room

--- moonshot-titan/levels/computer_core/computer_core.gd
+++ moonshot-titan/levels/computer_core/computer_core.gd
@@ -1,10 +1,10 @@
 extends QuestRoom
 
-class_name StorageRoom
+class_name ComputerCore
 
 var Quest = preload("res://levels/quest_room/quest.gd")
 
-onready var quest = Quest.new("Get Repair Materials", false)
+onready var quest = Quest.new("Initiate Launch Sequence", false)
 
 func _on_Entrance_body_entered(_body):
 	emit_signal("player_entered", quest, $PlayerRespawn)
