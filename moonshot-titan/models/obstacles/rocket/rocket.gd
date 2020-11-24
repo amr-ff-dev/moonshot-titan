28e8bdbd10fcb1db41c227d96d6fd5a33d84d0d8
AMR <73802610+amr-ff-dev@users.noreply.github.com>
2020-11-23 20:44:50 -0700

    remove rocket after launch animation finishes

--- moonshot-titan/models/obstacles/rocket/rocket.gd
+++ moonshot-titan/models/obstacles/rocket/rocket.gd
@@ -5,3 +5,4 @@ onready var completion_animation = $AnimationPlayer
 func _on_Interactable_interaction_complete():
 	completion_animation.play("launch_rocket")
 	yield(completion_animation, "animation_finished")
+	queue_free()
