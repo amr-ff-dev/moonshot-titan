3b86360d2668c93b5381772c23803788a803d989
AMR <73802610+amr-ff-dev@users.noreply.github.com>
2020-11-24 21:31:57 -0700

    #70 make rocket pad a quest room, make lighting more flexible

--- moonshot-titan/levels/quest_room/quest_room.gd
+++ moonshot-titan/levels/quest_room/quest_room.gd
@@ -6,15 +6,18 @@ class_name QuestRoom
 signal player_entered(quest, spawn_point)
 signal quest_complete(quest)
 
-export(Color, RGBA) var default_light_color = Color( 1, 0.196078, 0.196078, 1 )
+export(Color, RGBA) var start_light_color = Color( 1, 0.196078, 0.196078, 1 )
+export(Vector2) var light_position = Vector2(320, 384)
+export(float, 1.0, 3.0, 0.1) var light_scale = 1.8
 
+onready var light_texture = preload("res://effects/light.png")
 onready var light = Light2D.new()
 
 func _ready():
-	light.position = Vector2(320, 384)
-	light.texture = load("res://effects/light.png")
-	light.texture_scale = 1.8
-	light.color = default_light_color
+	light.color = start_light_color
+	light.position = light_position
+	light.texture_scale = light_scale
+	light.texture = light_texture
 	add_child(light)
 
 func emit_player_entered(quest, spawn_point):
