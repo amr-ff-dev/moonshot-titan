c817e93aca8d65f1176c9ee1d594d55cd13b7c36
mng <73802596+mng-ff-dev@users.noreply.github.com>
2020-11-29 17:32:42 -0700

    Revert "#95 polished mess hall, added objective and suit"
    This reverts commit 6d40835c8700831875fc9d2b95471092d763c98d.

--- moonshot-titan/levels/mess_hall/mess_hall.gd
+++ moonshot-titan/levels/mess_hall/mess_hall.gd
@@ -2,7 +2,7 @@ extends QuestRoom
 
 onready var entrance = $Entrance
 
-func _init().("Get Food Rations"):
+func _init().("Kill the Queen"):
 	pass
 
 func _on_Entrance_player_entered(_player):
@@ -12,7 +12,3 @@ func _on_Entrance_player_entered(_player):
 func finish_room():
 	.finish_room()
 	entrance.open_permanently()
-
-func _on_Interactable_interaction_complete():
-	emit_quest_complete()
-	$Interactable.queue_free()
