65014fae6e4565e11faff96f01c31619cf2a6a06
mng <73802596+mng-ff-dev@users.noreply.github.com>
2020-11-29 18:00:54 -0700

    #95 adjusted spider queen radius and re-attached interactable

--- moonshot-titan/levels/mess_hall/mess_hall.gd
+++ moonshot-titan/levels/mess_hall/mess_hall.gd
@@ -2,12 +2,16 @@ extends QuestRoom
 
 onready var entrance = $Entrance
 
-func _init().("Kill the Queen"):
+func _init().("Get Food Rations"):
 	pass
 
 func _on_Entrance_player_entered(_player):
 	emit_player_entered($PlayerRespawn)
 	emit_quest_active()
+
+func _on_Interactable_interaction_complete():
+	emit_quest_complete()
+	$Interactable.queue_free()
 
 func finish_room():
 	.finish_room()
