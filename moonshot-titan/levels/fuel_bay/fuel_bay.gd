b3c9306cf098653bfe5784b46fff88b794a6f1b6
mng <73802596+mng-ff-dev@users.noreply.github.com>
2020-11-29 17:06:45 -0700

    #83 tightened up the grav puzzle to encourage doing the mechanic

--- moonshot-titan/levels/fuel_bay/fuel_bay.gd
+++ moonshot-titan/levels/fuel_bay/fuel_bay.gd
@@ -12,13 +12,7 @@ func _on_Entrance_player_entered(_player):
 func _on_Interactable_interaction_complete():
 	emit_quest_complete()
 	$Interactable.queue_free()
-	# TODO: Remove finish_room when room is fleshed out
-	finish_room() 
 
 func finish_room():
 	.finish_room()
 	entrance.open_permanently()
-
-
-func _on_SuitLocker_locker_interaction_complete(texture):
-	pass # Replace with function body.
